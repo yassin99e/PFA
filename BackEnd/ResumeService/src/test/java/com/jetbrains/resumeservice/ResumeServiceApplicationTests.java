262607d764f9dbd51f92eb623f74a4489010a15b
Abbassi Zaid <lodstar43@gmail.com>
2025-05-10 08:52:28 +0100

    added ResumeService

---
+++ BackEnd/ResumeService/src/test/java/com/jetbrains/resumeservice/ResumeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.resumeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
